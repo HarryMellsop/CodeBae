bc34157f5a56acb218d818765dbe21e334669116
Thariq Ridha <tridha@roblox.com>
2021-04-19 19:17:59 -0700

    Modified completion provider to wait for server

--- frontend/src/extension.ts
+++ frontend/src/extension.ts
@@ -10,25 +10,24 @@ export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "codebae" is now active!');
-
-	// TODO: Change this to be whatever message we get from the backend
-	var message = "Hello World from Codebae";
 	
 	// Registering autocomplete item
 	const provider1 = vscode.languages.registerCompletionItemProvider('python', {
-		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-			
-			const messageCompletion = new vscode.CompletionItem("Hello World");
-			const messageCompletion2 = new vscode.CompletionItem("HI Josh");
+		async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+			let message : string = "";
 
 			//make get req to httpbin which just reflects the data
-			axios.get('https://httpbin.org/').then(response => {
+			// TODO: Change this request to ping server
+			await axios.get('https://httpbin.org/get').then(response => {
 				console.log(response.data);
+				message = response.data;
 			});
 
+			const messageCompletion = new vscode.CompletionItem(message);
+
 			return [
-				messageCompletion,
-				messageCompletion2
+				messageCompletion
 			];
 		}
 	});
