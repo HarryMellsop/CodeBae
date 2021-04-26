bb774bffbe11583f7ddd59659e03b9e8da74b64e
Joshua Lara <34728277+joshlara@users.noreply.github.com>
2021-04-25 21:52:26 -0700

    Merge frontend code to main - now with backend connection (#29)
    * now working with frontend connection: made changes to content-typeheader and using URLSearchParams to add post req data
    
    * code cleanup & comments

--- frontend/src/extension.ts
+++ frontend/src/extension.ts
@@ -1,7 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import axios from 'axios';
 import * as vscode from 'vscode';
+
+// Run 'npm install url'
+import { URLSearchParams } from 'url';
+// Run 'npm install axios'
+import axios from 'axios';
 
 /// this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,19 +18,36 @@ export function activate(context: vscode.ExtensionContext) {
 	// Registering autocomplete item
 	const provider1 = vscode.languages.registerCompletionItemProvider('python', {
 		async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
-
+			// Get text in current document and place current cursor position in as <cursor>
 			const beforeCursor = document.getText(new vscode.Range(new vscode.Position(0,0), position));
 			const afterCursor = document.getText(new vscode.Range(position, document.lineAt(document.lineCount - 1).range.end));
 			const docText : string = beforeCursor + "<cursor>" + afterCursor;
 			let message : string = "";
 
-			await axios.post('54.193.42.230:8080/predict', {current_file: docText}).then(response => {
-				console.log(response.data);
-				message = response.data;
-			});
+			// Create properly formatted body of POST request
+			let params = new URLSearchParams();
+			params.append('current_file', docText);
 
+			// Send POST request to backend
+			await axios.post('http://18.116.74.78:8080/predict', params, {
+				// Set Content-Type of request
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}})
+				.then(response => {
+					console.log(response.data);
+					message = response.data;
+				})
+				.catch(err => {
+					if (err.response) {
+						console.log('RESPONSE ERR: ' + err.message);
+					} else if (err.request) {
+						console.log('REQUEST ERR: ' + err.message);
+					} else {
+						console.log('ERR: ' + err.message);
+					}
+				});
 			const messageCompletion = new vscode.CompletionItem("H" + message);
-
 			return [
 				messageCompletion
 			];
